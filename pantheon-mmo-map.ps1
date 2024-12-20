5dff65feceaa54f255a032f13c863714df55acb9
Justin Giboney <justin_giboney@byu.edu>
2024-12-20 14:22:55 -0700

    Mostly working version

--- pantheon-mmo-map.ps1
+++ pantheon-mmo-map.ps1
@@ -37,7 +37,7 @@ $mapImage = "https://raw.githubusercontent.com/jgiboney/mmo-map/refs/heads/main/
             <Button x:Name="ZoomInButton" Content="+" Width="20" Height="20" Background="#333333" Foreground="White"/>
             <Button x:Name="ZoomOutButton" Content="-" Width="20" Height="20" Background="#333333" Foreground="White"/>
             <Button x:Name="UpdateLocationButton" Content="U" Width="20" Height="20" Background="DarkMagenta" Foreground="White"/>
-            <Button x:Name="AutoRunButton" Content="A" Width="20" Height="20" Background="DarkGreen" Foreground="White"/>
+            <!-- <Button x:Name="AutoRunButton" Content="A" Width="20" Height="20" Background="DarkGreen" Foreground="White"/> -->
         </StackPanel>
     </Grid>
 </Window>
@@ -66,35 +66,37 @@ $Window.FindName("CloseButton").Add_Click({
 # Zoom In Functionality
 $Window.FindName("ZoomInButton").Add_Click({
     if ($Window.Height+20 -lt [System.Windows.SystemParameters]::PrimaryScreenHeight) { # Check if the window height is less than the screen height
-        $Window.Height = $Window.Height + 20                # Increase the window height (from the bottom left corner)
-        $Window.Width = $Window.Width + 20                  # Increase the window width (from the bottom left corner)
-        $Window.Left = $Window.Left - 10.05                 # Move the window to the right
+        $Window.Height = $Window.Height + 100                # Increase the window height (from the bottom left corner)
+        $Window.Width = $Window.Width + 100                  # Increase the window width (from the bottom left corner)
+        $Window.Left = $Window.Left - 50.05                 # Move the window to the right
         $MyImage = $Window.FindName("MyImage")              # Get the Image control
-        $MyImage.Width += 20                                # Increase the image width
-        $MyImage.Height += 20                               # Increase the image height
-        $MapClip = $Window.FindName("MapClip")              # Get the clipping geometry
-        $HalfWidth = ($MyImage.Width)/2                     # Calculate the half width of the image
-        $MapClip.Center = $HalfWidth.ToString()+",125"     # Update the center of the clipping geometry
+        $MyImage.Width += 100                                # Increase the image width
+        $MyImage.Height += 100                               # Increase the image height
+        #$MapClip = $Window.FindName("MapClip")              # Get the clipping geometry
+        #$HalfWidth = ($MyImage.Width)/2                     # Calculate the half width of the image
+        #$MapClip.Center = $HalfWidth.ToString()+",125"     # Update the center of the clipping geometry
+        UpdateMap
     }
 })
 
 # Zoom Out Functionality
 $Window.FindName("ZoomOutButton").Add_Click({
     if ($Window.Height -gt 280) {                           # Check if the window height is greater than 280
-        $Window.Height = $Window.Height - 20                # Decrease the window height (from the bottom left corner)
-        $Window.Width = $Window.Width - 20                  # Decrease the window width (from the bottom left corner)
-        $Window.Left = $Window.Left + 10.05                 # Move the window to the left
+        $Window.Height = $Window.Height - 100                # Decrease the window height (from the bottom left corner)
+        $Window.Width = $Window.Width - 100                  # Decrease the window width (from the bottom left corner)
+        $Window.Left = $Window.Left + 50.05                 # Move the window to the left
         $MyImage = $Window.FindName("MyImage")              # Get the Image control
-        $MyImage.Width -= 20                                # Decrease the image width
-        $MyImage.Height -= 20                               # Decrease the image height
-        $MapClip = $Window.FindName("MapClip")              # Get the clipping geometry
-        $HalfWidth = ($MyImage.Width)/2                     # Calculate the half width of the image
-        $MapClip.Center = $HalfWidth.ToString()+",125"      # Update the center of the clipping geometry
+        $MyImage.Width -= 100                                # Decrease the image width
+        $MyImage.Height -= 100                               # Decrease the image height
+        #$MapClip = $Window.FindName("MapClip")              # Get the clipping geometry
+        #$HalfWidth = ($MyImage.Width)/2                     # Calculate the half width of the image
+        #$MapClip.Center = $HalfWidth.ToString()+",125"      # Update the center of the clipping geometry
+        UpdateMap
     }
 })
 
-# Update Location Functionality
-$Window.FindName("UpdateLocationButton").Add_Click({
+
+function UpdateMap () {
     # Get the current margin of the image
     $MyImage = $Window.FindName("MyImage")                  # Get the Image control
     $Margin = $MyImage.Margin                               # Get the current margin
@@ -103,39 +105,68 @@ $Window.FindName("UpdateLocationButton").Add_Click({
     $wshell.AppActivate('Pantheon') # Activate the Pantheon window
     # Type /loc
     $wshell.SendKeys('{ENTER}')
-    Start-Sleep -Milliseconds 50
+    Start-Sleep -Milliseconds 100
     $wshell.SendKeys('/loc')
-    Start-Sleep -Milliseconds 50
+    Start-Sleep -Milliseconds 100
     $wshell.SendKeys('{ENTER}')
-    Start-Sleep -Milliseconds 50
+    Start-Sleep -Milliseconds 100
     # Grab the clipboard
     $clipboard = (Get-Clipboard)
     # print the clipboard
-    Write-Host $clipboard
     # Split the clipboard into 5 values (name, x, z, y, heading)
     $locValues = $clipboard -split " "
-    Write-Host "X: $($locValues[1]) Y: $($locValues[3])"
     # Adjust the margin based on the x and y values
     # Current coords min = 2103.26, -4189.89 max = 4568.41, -1365.48
     # Get the percentage of the current location in relation to the map
-    $xPercent = ($locValues[1] - 2103.26) / (4568.41 - 2103.26)
-    $yPercent = ($locValues[3] - -4189.89) / (-1365.48 - -4189.89)
-    Write-Host "X: $xPercent Y: $yPercent"
+    $xPercent = ($locValues[1] - 2103.26) / (4568.41 - 2103.26) # Middle is 3335.835
+    $yPercent = ($locValues[3] - -4189.89) / (-1365.48 - -4189.89) # Middle is -2777.185
     $xPercent = $xPercent - .5
     $yPercent = $yPercent - .5
     # Calculate the new margin based on the percentage
     $newLeft = -($xPercent * $MyImage.Width)
-    $newTop = ($yPercent * $MyImage.Height)
-    Write-Host "Left: $newLeft Top: $newTop"
+    $yModifier = 15
+    $newTop = ($yPercent * $MyImage.Height) - $yModifier
     $MyImage.Margin = "$newLeft,$newTop,0,0"
-    Write-Host "Margin: $newLeft,$newTop,0,0"
+    # Update the clip
+    $MapClip = $Window.FindName("MapClip")              # Get the clipping geometry
+    $newCenter = (-$newLeft + $MyImage.Width/2).ToString()+","+(-$newTop-$yModifier + $MyImage.Height/2).ToString()     # Update the center of the clipping geometry
+    $MapClip.Center = $newCenter
+}
+
+# Update Location Functionality
+$Window.FindName("UpdateLocationButton").Add_Click({
+    UpdateMap
 })
 
-# Auto Run Functionality
-$Window.FindName("AutoRunButton").Add_Click({
-    $MyImage = $Window.FindName("MyImage")                  # Get the Image control
-    
-})
+$autoRun = $false
+$timer = New-Object System.Timers.Timer
+
+# # Auto Run Functionality
+# $Window.FindName("AutoRunButton").Add_Click({
+#     if ($global:autoRun -eq $true) {
+#         $autoRun = $false
+#         Write-Host "Auto Run Disabled"
+#         $Window.FindName("AutoRunButton").Background = "DarkGreen"
+#         # Stop the timer
+#         $t = $global:timer
+#         $t.Stop()
+#     } else {
+#         $global:autoRun = $true
+#         Write-Host "Auto Run Enabled"
+#         $Window.FindName("AutoRunButton").Background = "LightGreen"
+#         # Update the map every 5 seconds
+#         $t = $global:timer
+#         $t.Interval = 5000
+#         $t.AutoReset = $true
+#         $t.Enabled = $true
+#         $t.add_Elapsed({
+#             Write-Host "Updating Map"
+#             UpdateMap
+#         })
+#         # Start the timer
+#         $t.Start()
+#     }
+# })
 
 # Load the image into the Image control
 $imageUrl = $mapImage  # Specify your image file path
